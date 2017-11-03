e6f53150fb9f89e031b0117d423c0dfbba17ea69
dmall <wanli.zhang@dmall.com>
2017-11-04 04:11:23 +0800

    add log

--- src/main/java/zwl/learning/note/aop/demo/TimeHandler.java
+++ src/main/java/zwl/learning/note/aop/demo/TimeHandler.java
@@ -1,12 +1,16 @@
 package zwl.learning.note.aop.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author zhangwanli
  * @description
  * @date 2017-10-24 上午11:48
  */
 public class TimeHandler {
+    public static final Logger LOGGER = LoggerFactory.getLogger(TimeHandler.class);
     public void printTime() {
-        System.out.println("CurrentTime = " + System.currentTimeMillis());
+        LOGGER.info("CurrentTime = " + System.currentTimeMillis());
     }
 }
--- src/test/java/zwl/learning/note/aop/AOPTest.java
+++ src/test/java/zwl/learning/note/aop/AOPTest.java
@@ -46,12 +46,10 @@ public class AOPTest {
         HelloWorld hw2 = (HelloWorld) ctx.getBean("helloWorldImpl2");
 
         hw1.printHelloWorld();
-        System.out.println();
+
         hw1.doPrint();
 
-        System.out.println();
         hw2.printHelloWorld();
-        System.out.println();
         hw2.doPrint();
     }
 }
